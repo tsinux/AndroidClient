41b57e21b72103a43ed841517867895a3f9488fc
tsinu <tsinu@qq.com>
2016-05-13 11:44:56 +0800

    添加Student.java

---
+++ app/src/main/java/pub/iyu/androidclient/model/Student.java
@@ -0,0 +1,13 @@
+package pub.iyu.androidclient.model;
+
+/**
+ * Created by tsinu on 2016/5/13.
+ */
+public class Student {
+
+    private int id;
+    private String name;
+    private String classes;
+
+    
+}
